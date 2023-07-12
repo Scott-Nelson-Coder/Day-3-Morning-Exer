eb2c9e6e1c6b1536e01da81e11d0536832c610e2
Scott Nelson <scottninlv@yahoo.com>
2023-07-12 07:43:33 -0700

    day 3 morning lab

---
+++ maxOfTwo.js
@@ -0,0 +1,16 @@
+let x = 11
+let y = 10
+
+// Write your code below this line.
+
+if (x > y) {
+    console.log(x)
+}
+else if (y > x) {
+    console.log(y)
+}
+else {
+    console.log("They're equal!")
+}
+
+
